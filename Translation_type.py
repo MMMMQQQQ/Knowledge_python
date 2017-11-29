f171da5c1b770ec5463a15660eb2aaa326c0134e
MMMMQQQQ <qiumingming7@gmail.com>
2017-11-29 14:56:37 +0100

    List and String translation

---
+++ Translation_type.py
@@ -0,0 +1,15 @@
+# list to string, combine all the list to one
+original=['1','2','1']
+result=''.join(original)
+print(result)
+print(result.index("1"))
+
+
+# list to string
+original=['xiao ming','2','1']
+print(original[1])
+print(original[0].find("i"))
+
+
+
+
